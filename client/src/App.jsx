e27afbb1e996482fbd8cd90ca3dc81b6c90d44c5
ctrlaltaf <altafayyubibarelvi@gmail.com>
2023-09-14 11:12:42 -0700

    Cleaned up CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState, useEffect} from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  <div>
-    <h1>Hello world</h1>
-  </div>
-  )
+    <div>
+      <div className="navbar">
+        <h1>Bio-Net-Viz</h1>
+        <ul className="navbar-menu">
+          <li>
+            <Link to={`/testing`}>Testing</Link>
+          </li>
+          <li>
+            <Link to={`/about`}>About</Link>
+          </li>
+        </ul>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Root from './routes/root.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./errorPage.jsx";
-import Movie from './components/Movie.jsx';
-import About from './components/About.jsx';
-
+import Movie from "./components/Movie.jsx";
+import About from "./components/About.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
