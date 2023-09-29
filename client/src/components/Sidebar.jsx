5b39d786c4df99ece219ad21eb7796eabbd95dda
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2023-09-29 16:28:31 -0700

    Add conditional rendering of ontological relationship

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -30,6 +30,48 @@ export default function Sidebar({ currentNode, sourceNode, log, goTerm }) {
         </div>
     </div>
         )
+    } else if (currentNode.type === "go_protein") {
+        return (
+            <div>
+                <div 
+                id="sidebarContent"
+                className="sidebar"
+                >
+                    <h2>Network Data</h2>
+                    <p>Protein: {currentNode.label}</p>
+                    <p>Database ID:&nbsp;
+                        <a
+                        className="sidebar-link"
+                        href={`https://flybase.org/reports/${currentNode.id}`}
+                        target="_blank"
+                        rel="noopener"
+                        >
+                            {currentNode.id}
+                        </a>
+                    </p>
+                    {/* In the future, we will want to cast the ID to UPPERCASE */}
+                    <p>Type of node: {currentNode.type}</p>
+                    <p>Protein of interest: {sourceNode}</p>
+                    <p>Source node GO terms: </p>
+                    <p>Queried GO term:&nbsp;
+                        <a
+                        className="sidebar-link"
+                        href={`https://amigo.geneontology.org/amigo/term/${goTerm}`}
+                        target="_blank"
+                        rel="noopener"
+                        >
+                            {goTerm}
+                        </a>
+                    </p>
+                    <p>Relationship to GO term: {currentNode.go_protein}</p>
+                    {/* Need a separate query in Cypher to get all GO terms for the sourceNode and then display them */}
+                    <ExportJSON 
+                    log = {log}
+                    />
+                </div>
+            </div>
+            );
+    
     } else {
 
     // need to write conditional logic to check the current node
@@ -38,44 +80,44 @@ export default function Sidebar({ currentNode, sourceNode, log, goTerm }) {
     // if currentNode.type === "intermediate" then display specific information about the path its on, maybe source and targets of path
     // if currentNode.type === "go_protein" then display specific information about the go term and level of evidence node
 
-    return (
-    <div>
-        <div 
-        id="sidebarContent"
-        className="sidebar"
-        >
-            <h2>Network Data</h2>
-            <p>Protein: {currentNode.label}</p>
-            <p>Database ID:&nbsp;
-                <a
-                className="sidebar-link"
-                href={`https://flybase.org/reports/${currentNode.id}`}
-                target="_blank"
-                rel="noopener"
-                >
-                    {currentNode.id}
-                </a>
-            </p>
+        return (
+        <div>
+            <div 
+            id="sidebarContent"
+            className="sidebar"
+            >
+                <h2>Network Data</h2>
+                <p>Protein: {currentNode.label}</p>
+                <p>Database ID:&nbsp;
+                    <a
+                    className="sidebar-link"
+                    href={`https://flybase.org/reports/${currentNode.id}`}
+                    target="_blank"
+                    rel="noopener"
+                    >
+                        {currentNode.id}
+                    </a>
+                </p>
             {/* In the future, we will want to cast the ID to UPPERCASE */}
-            <p>Type of node: {currentNode.type}</p>
-            <p>Protein of interest: {sourceNode}</p>
-            <p>Source node GO terms: </p>
-            <p>Queried GO term:&nbsp;
-                <a
-                className="sidebar-link"
-                href={`https://amigo.geneontology.org/amigo/term/${goTerm}`}
-                target="_blank"
-                rel="noopener"
-                >
-                    {goTerm}
-                </a>
-            </p>
+                <p>Type of node: {currentNode.type}</p>
+                <p>Protein of interest: {sourceNode}</p>
+                <p>Source node GO terms: </p>
+                <p>Queried GO term:&nbsp;
+                    <a
+                    className="sidebar-link"
+                    href={`https://amigo.geneontology.org/amigo/term/${goTerm}`}
+                    target="_blank"
+                    rel="noopener"
+                    >
+                        {goTerm}
+                    </a>
+                </p>
             {/* Need a separate query in Cypher to get all GO terms for the sourceNode and then display them */}
-            <ExportJSON 
-            log = {log}
-            />
+                <ExportJSON 
+                log = {log}
+                />
+            </div>
         </div>
-    </div>
-    );
+        );
     };
 };
