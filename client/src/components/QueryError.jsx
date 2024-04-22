92d87676ceb7ac66a76a76bdfd701523c3a7c473
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2024-04-22 15:20:11 -0700

    Add ability for users to file GitHub issue

--- client/src/components/QueryError.jsx
+++ client/src/components/QueryError.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { MdError } from "react-icons/md";
+import { React, useState } from "react";
+import { MdError, MdBugReport } from "react-icons/md";
 import { IconContext } from "react-icons";
 
 export default function QueryError({ errorMessage }) {
+  const [showBugReportMessage, setShowBugReportMessage] = useState(false);
+
   return (
     <div className="query-error">
       <IconContext.Provider
         value={{
-          className: "icon",
           size: "3em",
-          color:"black"
+          color: "black"
         }}
       >
         {" "}
@@ -19,6 +20,31 @@ export default function QueryError({ errorMessage }) {
         We encountered an error while retrieving the network. <br></br> Reason:{" "}
         {errorMessage}
       </h3>
-    </div>
+      <IconContext.Provider
+        value={{
+          size: "2em",
+          color: showBugReportMessage ? "grey" : "black",
+        }}
+      >
+        <div
+          className="bug-report"
+          onMouseEnter={() => setShowBugReportMessage(true)}
+          onMouseLeave={() => setShowBugReportMessage(false)}
+        >
+          <a
+            href="https://github.com/Reed-CompBio/protein-weaver/issues/new"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MdBugReport />
+          </a>
+          {showBugReportMessage &&
+            <div className="bug-report-message">
+              File a GitHub issue! (opens new tab)
+            </div>
+          }
+        </div>
+      </IconContext.Provider>
+    </div >
   );
 }
