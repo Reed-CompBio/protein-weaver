801b6a4b98bd70c4d90a1a21fdcf5ad8aac13e07
Aden O'Brien <adenjohnobrien@gmail.com>
2024-07-25 16:24:16 -0600

    revert parser changes

--- client/src/tools/Parser.jsx
+++ client/src/tools/Parser.jsx
@@ -1,21 +1,10 @@
-/**
- * Parser for handling API response data from Neo4j's Yen's Shortest Path Algorithm.
- *
- * @param {JSON} data   JSON data containing all the k shortest paths
- * @param {string} source   Protein of Interest
- * @param {string} go_term   GO term of Interest
- * @returns {JSON}
- */
-
-import { PanelResizeHandle } from "react-resizable-panels";
-
-
-// tag::NetworkParser
-export function NetworkParserPath(data, source) {
+export function NetworkParserPath(data, source, go_term) {
   let parsedData = { nodes: [], edges: [], nodeList: [], edgeList: [] };
   //Iterate through data where each element is a path
   for (let i = 0; i < data.length; i++) {
     let currentPath = data[i]._fields[4];
+    let startNode = null;
+    let endNode = null;
     for (let j = 0; j < currentPath.length - 1; j++) {
       //Add each node in a path, and label them accordingly (source, go_protein, or intermediate)
       //Keep track of all the nodes in nodeList
@@ -50,6 +39,21 @@ export function NetworkParserPath(data, source) {
         parsedData.nodes.push(nodeEntry);
       }
     }
+    for (let j = 1; j < currentPath.length - 1; j++) {
+      //Add the edges in a path and keep track in the edgeList
+      startNode = currentPath[j - 1].properties.id;
+      endNode = currentPath[j].properties.id;
+      if (
+        !parsedData.edgeList.includes(startNode + endNode) &&
+        !parsedData.edgeList.includes(endNode + startNode)
+      ) {
+        let edgeEntry = {
+          data: { source: endNode, target: startNode },
+        };
+        parsedData.edgeList.push(startNode + endNode);
+        parsedData.edges.push(edgeEntry);
+      }
+    }
   }
   parsedData.goTerm =
     data[0]._fields[data[0]._fields.length - 1][
@@ -57,7 +61,6 @@ export function NetworkParserPath(data, source) {
     ].properties;
   return parsedData;
 }
-
 /**
  * Parser that handles API response data from Neo4j all edges in an induced subgraph query.
  * Adds shared edge information and Protein to GO Term relationship properties
@@ -72,39 +75,42 @@ export function EdgeDataParser(networkData, edgeData) {
   for (let i = 0; i < edgeData.length; i++) {
     let startNode = edgeData[i]._fields[0].start.properties.id;
     let endNode = edgeData[i]._fields[0].end.properties.id;
-    let type = edgeData[i]._fields[0].segments[0].relationship.type;
-    //If the edge is a Protein to GO term relationship:
-    //Iterate through all the nodes in the nodeList and add the relationship information to the protein
-    if (type === "ProGo") {
+    //check for shared edges
+    console.log("looking at edge", startNode + endNode)
+    if (networkData.edgeList.includes(startNode + endNode)) {
+      console.log("found edge")
+    }
+    if (
+      !networkData.edgeList.includes(startNode + endNode) &&
+      !networkData.edgeList.includes(endNode + startNode) &&
+      edgeData[i]._fields[0].segments[0].relationship.type != "ProGo"
+    ) {
+      let edgeEntry = {
+        data: { source: endNode, target: startNode, type: "shared" },
+      };
+      networkData.edgeList.push(startNode + endNode);
+      networkData.edges.push(edgeEntry);
+    }
+    //If the edge is a Protein to GO term relationship,
+    //iterate through all the nodes in the nodeList and add the relationship information to the protein
+    else if (edgeData[i]._fields[0].segments[0].relationship.type === "ProGo") {
       for (let k = 0; k < networkData.nodes.length; k++) {
         let currentNode = networkData.nodes[k];
         if (currentNode.data.id === startNode) {
           networkData.nodes[k].data.go_protein =
-            edgeData[i]._fields[0].segments[0].relationship.properties.relationship;
+            edgeData[
+              i
+            ]._fields[0].segments[0].relationship.properties.relationship;
         }
-      }
-    }
-    //If the edge is not a Protein to GO term relationship:
-    //Check if the edge is already stored in networkData. If not, add the edge to networkData.edgeList
-    else {
-      if (
-        //--------doesnt work for multiple regulatory relationships--------
-        !networkData.edgeList.includes(startNode + endNode + type) &&
-        !networkData.edgeList.includes(endNode + startNode + type)) {
-        networkData.edgeList.push(startNode + endNode + type)
-        networkData.edges.push({ data: { source: endNode, target: startNode, type: type } })
-        // networkData.edges.push({ data: { source: endNode, target: startNode, type: type, tst: "shared" } })
       }
     }
   }
   return networkData;
 }
-
 export function NetworkParserNode(data, source, k) {
   let parsedData = { nodes: [], edges: [], nodeList: [], edgeList: [] };
   for (let i = 0; i < Math.min(k, data.length - 1); i++) {
     let currentPath = data[i];
-    // console.log("current path", data[i]);
     for (let j = 0; j < currentPath.length; j++) {
       //Add each node in a path, and label them accordingly (source, go_protein, or intermediate)
       //Keep track of all the nodes in nodeList
@@ -137,6 +143,23 @@ export function NetworkParserNode(data, source, k) {
       if (!parsedData.nodeList.includes(currentPath[j].properties.id)) {
         parsedData.nodeList.push(currentPath[j].properties.id);
         parsedData.nodes.push(nodeEntry);
+      }
+    }
+    let startNode = null;
+    let endNode = null;
+    for (let j = 1; j < currentPath.length; j++) {
+      //Add the edges in a path and keep track in the edgeList
+      startNode = currentPath[j - 1].properties.id;
+      endNode = currentPath[j].properties.id;
+      if (
+        !parsedData.edgeList.includes(startNode + endNode) &&
+        !parsedData.edgeList.includes(endNode + startNode)
+      ) {
+        let edgeEntry = {
+          data: { source: endNode, target: startNode },
+        };
+        parsedData.edgeList.push(startNode + endNode);
+        parsedData.edges.push(edgeEntry);
       }
     }
   }
