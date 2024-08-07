2cdcce3083aa1a11059a7e27307aa1aa38254a99
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-08-07 10:28:11 -0700

    fixed shared edge and reg only node bug

--- client/src/tools/Parser.jsx
+++ client/src/tools/Parser.jsx
@@ -6,32 +6,35 @@ export function NetworkParserPath(data, source, go_term) {
         let startNode = null;
         let endNode = null;
         for (let j = 0; j < currentPath.length - 1; j++) {
+            let nodeName = null;
+            let nodeId = currentPath[j].properties.id;
+            let nodeAltName = currentPath[j].properties.alt_name;
+            // handles the case where name param doesnt exist. representing node that only has regulatory interactions
+            if (currentPath[j].properties.name != null) {
+                nodeName = currentPath[j].properties.name;
+            } else {
+                nodeName = currentPath[j].properties.gene_name;
+            }
             //Add each node in a path, and label them accordingly (source, go_protein, or intermediate)
             //Keep track of all the nodes in nodeList
             let nodeEntry = {
                 data: {
-                    id: currentPath[j].properties.id,
-                    label: currentPath[j].properties.name,
+                    id: nodeId,
+                    label: nodeName,
                     degree: currentPath[j].properties.degree.low,
                 },
             };
             if (
-                (currentPath[j].properties.name.toUpperCase() ===
-                    source.toUpperCase() ||
-                    currentPath[j].properties.id.toUpperCase() ===
-                    source.toUpperCase() ||
-                    currentPath[j].properties.alt_name.toUpperCase() ===
-                    source.toUpperCase()) &&
+                (nodeName.toUpperCase() === source.toUpperCase() ||
+                    nodeId.toUpperCase() === source.toUpperCase() ||
+                    nodeAltName.toUpperCase() === source.toUpperCase()) &&
                 j == currentPath.length - 2
             ) {
                 nodeEntry.data.type = "go_source";
             } else if (
-                currentPath[j].properties.name.toUpperCase() ===
-                source.toUpperCase() ||
-                currentPath[j].properties.id.toUpperCase() ===
-                source.toUpperCase() ||
-                currentPath[j].properties.alt_name.toUpperCase() ===
-                source.toUpperCase()
+                nodeName.toUpperCase() === source.toUpperCase() ||
+                nodeId.toUpperCase() === source.toUpperCase() ||
+                nodeAltName.toUpperCase() === source.toUpperCase()
             ) {
                 nodeEntry.data.type = "source";
             } else if (j == currentPath.length - 2) {
@@ -39,8 +42,8 @@ export function NetworkParserPath(data, source, go_term) {
             } else {
                 nodeEntry.data.type = "intermediate";
             }
-            if (!parsedData.nodeList.includes(currentPath[j].properties.id)) {
-                parsedData.nodeList.push(currentPath[j].properties.id);
+            if (!parsedData.nodeList.includes(nodeId)) {
+                parsedData.nodeList.push(nodeId);
                 parsedData.nodes.push(nodeEntry);
             }
         }
@@ -81,63 +84,33 @@ export function NetworkParserPath(data, source, go_term) {
 // tag::EdgeDataParser
 export function EdgeDataParser(networkData, edgeData) {
     //Iterate through al the edges in the induced subgraph
+    // console.log(edgeData)
     let tempEdgeList = [];
     let tempEdges = [];
     for (let i = 0; i < edgeData.length; i++) {
         let startNode = edgeData[i]._fields[0].start.properties.id;
         let endNode = edgeData[i]._fields[0].end.properties.id;
         let relType = edgeData[i]._fields[0].segments[0].relationship.type;
-        let pubmed = edgeData[i]._fields[0].segments[0].relationship.properties.pubmed;
-        let link = edgeData[i]._fields[0].segments[0].relationship.properties.link;
-        let fbRef = edgeData[i]._fields[0].segments[0].relationship.properties.reference;
         //Check for shared edges
         //If the edge already exists in the initial network data, add it to the temp edge list
         if (
             networkData.edgeList.includes(endNode + startNode) ||
             networkData.edgeList.includes(startNode + endNode)
         ) {
-            if (relType === ("ProPro" || "Reg")) {
-                if (pubmed) {
-                    let edgeEntry = {
-                        data: {
-                            source: endNode,
-                            target: startNode,
-                            relType: relType,
-                            evidence: pubmed,
-                        },
-                    };
-                    tempEdgeList.push(startNode + endNode);
-                    tempEdges.push(edgeEntry);
-                }
-                else if (link) {
-                    let edgeEntry = {
-                        data: {
-                            source: endNode,
-                            target: startNode,
-                            relType: relType,
-                            evidence: link,
-                        },
-                    };
-                    tempEdgeList.push(startNode + endNode);
-                    tempEdges.push(edgeEntry);
-                }
-                else if (fbRef) {
-                    let edgeEntry = {
-                        data: {
-                            source: endNode,
-                            target: startNode,
-                            relType: relType,
-                            evidence: fbRef,
-                        },
-                    };
-                    tempEdgeList.push(startNode + endNode);
-                    tempEdges.push(edgeEntry);
-                }
-            }
+            let edgeEntry = {
+                data: {
+                    source: endNode,
+                    target: startNode,
+                    relType: relType,
+                    evidence: "evidenceTest",
+                },
+            };
+            tempEdgeList.push(startNode + endNode);
+            tempEdges.push(edgeEntry);
         }
         //If the edge type is ProGo, add the edges relationship properties to the network data
         else if (
-            relType === "ProGo"
+            edgeData[i]._fields[0].segments[0].relationship.type === "ProGo"
         ) {
             for (let k = 0; k < networkData.nodes.length; k++) {
                 let currentNode = networkData.nodes[k];
@@ -151,43 +124,17 @@ export function EdgeDataParser(networkData, edgeData) {
         }
         //If an edge is found that was not a part of the inital network data, add it to the temp edge list with the shared tag
         else {
-            if (pubmed) {
-                let edgeEntry = {
-                    data: {
-                        source: endNode,
-                        target: startNode,
-                        type: "shared",
-                        relType: relType,
-                        evidence: pubmed,
-                    },
-                };
-                tempEdgeList.push(startNode + endNode);
-                tempEdges.push(edgeEntry);
-            } else if (link) {
-                let edgeEntry = {
-                    data: {
-                        source: endNode,
-                        target: startNode,
-                        type: "shared",
-                        relType: relType,
-                        evidence: link,
-                    },
-                };
-                tempEdgeList.push(startNode + endNode);
-                tempEdges.push(edgeEntry);
-            } else if (fbRef) {
-                let edgeEntry = {
-                    data: {
-                        source: endNode,
-                        target: startNode,
-                        type: "shared",
-                        relType: relType,
-                        evidence: fbRef,
-                    },
-                };
-                tempEdgeList.push(startNode + endNode);
-                tempEdges.push(edgeEntry);
-            }
+            let edgeEntry = {
+                data: {
+                    source: endNode,
+                    target: startNode,
+                    type: "shared",
+                    relType: relType,
+                    evidence: "evidenceTest",
+                },
+            };
+            tempEdgeList.push(startNode + endNode);
+            tempEdges.push(edgeEntry);
         }
     }
     networkData.edgeList = tempEdgeList;
@@ -201,30 +148,33 @@ export function NetworkParserNode(data, source, k) {
         for (let j = 0; j < currentPath.length; j++) {
             //Add each node in a path, and label them accordingly (source, go_protein, or intermediate)
             //Keep track of all the nodes in nodeList
+            let nodeName = null;
+            let nodeId = currentPath[j].properties.id;
+            let nodeAltName = currentPath[j].properties.alt_name;
+            //If the edge already exists in the initial network data, add it to the temp edge list\
+            if (currentPath[j].properties.name != null) {
+                nodeName = currentPath[j].properties.name;
+            } else {
+                nodeName = currentPath[j].properties.gene_name;
+            }
             let nodeEntry = {
                 data: {
-                    id: currentPath[j].properties.id,
-                    label: currentPath[j].properties.name,
+                    id: nodeId,
+                    label: nodeName,
                     degree: currentPath[j].properties.degree.low,
                 },
             };
             if (
-                (currentPath[j].properties.name.toUpperCase() ===
-                    source.toUpperCase() ||
-                    currentPath[j].properties.id.toUpperCase() ===
-                    source.toUpperCase() ||
-                    currentPath[j].properties.alt_name.toUpperCase() ===
-                    source.toUpperCase()) &&
+                (nodeName.toUpperCase() === source.toUpperCase() ||
+                    nodeId.toUpperCase() === source.toUpperCase() ||
+                    nodeAltName.toUpperCase() === source.toUpperCase()) &&
                 j == currentPath.length - 1
             ) {
                 nodeEntry.data.type = "go_source";
             } else if (
-                currentPath[j].properties.name.toUpperCase() ===
-                source.toUpperCase() ||
-                currentPath[j].properties.id.toUpperCase() ===
-                source.toUpperCase() ||
-                currentPath[j].properties.alt_name.toUpperCase() ===
-                source.toUpperCase()
+                nodeName.toUpperCase() === source.toUpperCase() ||
+                nodeId.toUpperCase() === source.toUpperCase() ||
+                nodeAltName.toUpperCase() === source.toUpperCase()
             ) {
                 nodeEntry.data.type = "source";
             } else if (j == currentPath.length - 1) {
@@ -232,8 +182,8 @@ export function NetworkParserNode(data, source, k) {
             } else {
                 nodeEntry.data.type = "intermediate";
             }
-            if (!parsedData.nodeList.includes(currentPath[j].properties.id)) {
-                parsedData.nodeList.push(currentPath[j].properties.id);
+            if (!parsedData.nodeList.includes(nodeId)) {
+                parsedData.nodeList.push(nodeId);
                 parsedData.nodes.push(nodeEntry);
             }
         }
