1fd35f8d00db01dc583ac018832610c0815c1414
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2024-08-08 14:50:00 -0700

    Refactor Edges tab

--- client/src/components/StatisticsTab.jsx
+++ client/src/components/StatisticsTab.jsx
@@ -4,6 +4,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import Motif from "./Motif";
 import NodeTab from "./NodeTab";
+import EdgeTab from "./EdgeTab";
 
 export default function StatisticsTab({
     nodeList,
@@ -15,41 +16,6 @@ export default function StatisticsTab({
     query,
 }) {
     const [tabIndex, setTabIndex] = useState(0);
-    const [interactionDatabase, setInteractionDatabase] = useState("");
-
-    useEffect(() => {
-        getDatabase(edgeEvidence);
-    }, [edgeEvidence]);
-
-    const getDatabase = (evidence) => {
-        if (evidence.startsWith("FBrf")) {
-            setInteractionDatabase("FlyBase");
-        } else if (evidence.includes("/")) {
-            setInteractionDatabase("STRING");
-        } else if (/^\d{1,8}$/.test(evidence)) {
-            setInteractionDatabase("PMID");
-        } else if (evidence.includes(";")) {
-            setInteractionDatabase("PMID");
-        } else {
-            setInteractionDatabase("");
-        }
-    }
-
-    const getLink = (evidence) => {
-        if (evidence.startsWith("FBrf")) {
-            return [`https://flybase.org/reports/${evidence}.htm`];
-        } else if (evidence.includes("/")) {
-            return [`https://string-db.org/interaction/${evidence}`];
-        } else if (/^\d{1,8}$/.test(evidence)) {
-            return [`https://pubmed.ncbi.nlm.nih.gov/${evidence}`]
-        } else if (evidence.includes(";")) {
-            return evidence.split(";").map(i => `https://pubmed.ncbi.nlm.nih.gov/${i}`);
-        } else {
-            return [];
-        }
-    };
-
-    const edgeLink = getLink(edgeEvidence);
 
     return (
         <div className="statistics-panel-container">
@@ -73,37 +39,12 @@ export default function StatisticsTab({
                 </TabPanel>
                 <TabPanel>
                     <h4 className="stats-title">Edges stats</h4>
-                    <div>
-                        <div className="click-edge-container">
-                            <h4>
-                                <div>
-                                    Selected edge:{" "}
-                                    {edgeLink.length > 0 ? (
-                                        edgeLink.map((link, index) => (
-                                            <span key={index}>
-                                                <a href={link} target="_blank" rel="noopener noreferrer">
-                                                    {edgeEvidence.split(";")[index]}
-                                                </a>
-                                                {` (${interactionDatabase})`}
-                                                {index < edgeLink.length - 1 && "; "}
-                                            </span>
-                                        ))
-                                    ) : (
-                                        edgeEvidence
-                                    )}
-                                </div>
-                            </h4>
-                        </div>
-                        {edgeSource ? (
-                            <div className="edge-container">
-                                <div>Source node: {edgeSource}</div>
-                                <div>Target edge: {edgeTarget}</div>
-                                <div>Edge type: {edgeType}</div>
-                            </div>
-                        ) : (
-                            <p>Click an edge to see more information</p>
-                        )}
-                    </div>
+                    <EdgeTab
+                        edgeEvidence={edgeEvidence}
+                        edgeSource={edgeSource}
+                        edgeTarget={edgeTarget}
+                        edgeType={edgeType}
+                    />
                 </TabPanel>
                 <TabPanel>
                     <div className="graph-statistics-container">
