e817eea518712e28354e102bfa07b7154f286f1b
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2024-08-09 16:18:05 -0700

    Update NodeTab.jsx
    make code more concise

--- client/src/components/NodeTab.jsx
+++ client/src/components/NodeTab.jsx
@@ -9,90 +9,61 @@ export default function NodeTab({
 
     // Change the Database link based on species and selected protein
     useEffect(() => {
-        if (currentNode) {
-            if (query.species === "txid7227") {
-                setSelectedDbLink(`https://flybase.org/reports/${currentNode.id}`);
-            } else if (query.species === "txid224308") {
-                setSelectedDbLink(`https://bsubcyc.org/gene?orgid=BSUB&id=${currentNode.id.replace("_", "")}`);
-            } else if (query.species === "txid7955") {
-                setSelectedDbLink(`https://www.uniprot.org/uniprotkb/${currentNode.id}/entry`);
-            }
+        if (currentNode && query.species) {
+            const speciesLinkMap = {
+                "txid7227": `https://flybase.org/reports/${currentNode.id}.htm`,
+                "txid224308": `https://bsubcyc.org/gene?orgid=BSUB&id=${currentNode.id.replace("_", "")}`,
+                "txid7955": `https://www.uniprot.org/uniprotkb/${currentNode.id}/entry`,
+            };
+            setSelectedDbLink(speciesLinkMap[query.species] || "");
         }
     }, [currentNode, query.species]);
+
     if (!currentNode) {
-        return (
-            <p>Select a protein</p>
-        )
-    } else if (currentNode.type === "intermediate") {
-        return (
-            <div className="protein-summary">
-                <h5>Selected protein: {currentNode.label}
-                    <Degree id={currentNode.id} />
-                </h5>
-                <p className="database-link">
-                    Database link:&nbsp;
-                    <a
-                        className="tan-sidebar-link"
-                        href={selectedDbLink}
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        {currentNode.id}
-                    </a>
-                </p>
-            </div>
-        )
-    } else if (currentNode.type === "source") {
-        return (
-            <div className="protein-summary">
-                <h5>Selected protein: {currentNode.label}
-                    <Degree id={currentNode.id} />
-                </h5>
-                <p className="database-link">
-                    Database link:&nbsp;
-                    <a
-                        className="red-sidebar-link"
-                        href={selectedDbLink}
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        {currentNode.id}
-                    </a>
-                </p>
-            </div>
-        )
-    } else if (
-        currentNode.type === "go_protein" ||
-        currentNode.type === "go_source"
-    ) {
-        return (
-            <div className="protein-summary">
-                <h5>Selected protein: {currentNode.label}
-                    <Degree id={currentNode.id} />
-                </h5>
-                <p className="database-link">
-                    Database link:&nbsp;
-                    <a
-                        className="blue-sidebar-link"
-                        href={selectedDbLink}
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        {currentNode.id}
-                    </a>
-                </p>
-                <p className="go-protein-disclaimer">*This protein is annotated to the queried GO term with:</p>
-                <p
-                    style={{
-                        fontWeight:
-                            currentNode.go_protein === "inferred_from_descendant"
-                                ? "bold"
-                                : "normal",
-                    }}
-                >
-                    {currentNode.go_protein}
-                </p>
-            </div>
-        )
+        return <p>Select a protein</p>;
     }
+
+    const linkClassMap = {
+        "intermediate": "tan-sidebar-link",
+        "source": "red-sidebar-link",
+        "go_protein": "blue-sidebar-link",
+        "go_source": "blue-sidebar-link",
+    };
+
+    return (
+        <div className="protein-summary">
+            <h5>
+                Selected protein: {currentNode.label}
+                <Degree id={currentNode.id} />
+            </h5>
+            <p className="database-link">
+                Database link:&nbsp;
+                <a
+                    className={linkClassMap[currentNode.type] || ""}
+                    href={selectedDbLink}
+                    target="_blank"
+                    rel="noopener"
+                >
+                    {currentNode.id}
+                </a>
+            </p>
+            {(currentNode.type === "go_protein" || currentNode.type === "go_source") && (
+                <>
+                    <p className="go-protein-disclaimer">
+                        *This protein is annotated to the queried GO term with:
+                    </p>
+                    <p
+                        style={{
+                            fontWeight:
+                                currentNode.go_protein === "inferred_from_descendant"
+                                    ? "bold"
+                                    : "normal",
+                        }}
+                    >
+                        {currentNode.go_protein}
+                    </p>
+                </>
+            )}
+        </div>
+    );
 } 
